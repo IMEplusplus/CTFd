554acb63abbc3fcb3054bbf5d457c89ef5e0a336
root <root@localhost.localdomain>
2018-02-15 09:06:14 -0500

    Altera o favicon.ico e logo.png

--- CTFd/views.py
+++ CTFd/views.py
@@ -42,11 +42,15 @@ def setup():
     <div class="col-md-6 offset-md-3">
         <img class="w-100 mx-auto d-block" style="max-width: 500px;padding: 50px;padding-top: 14vh;" src="themes/core/static/img/logo.png" />
         <h3 class="text-center">
-            <p>IMECTF 0x02 - Processo seletivo</a></p>
+            <p>A cool CTF platform from <a href="https://ctfd.io">ctfd.io</a></p>
+            <p>Follow us on social media:</p>
+            <a href="https://twitter.com/ctfdio"><i class="fab fa-twitter fa-2x" aria-hidden="true"></i></a>&nbsp;
+            <a href="https://facebook.com/ctfdio"><i class="fab fa-facebook fa-2x" aria-hidden="true"></i></a>&nbsp;
+            <a href="https://github.com/ctfd"><i class="fab fa-github fa-2x" aria-hidden="true"></i></a>
         </h3>
         <br>
         <h4 class="text-center">
-            <a href="admin">Cadastre-se</a> e comece a solucionar os problemas
+            <a href="admin">Click here</a> to login and setup your CTF
         </h4>
     </div>
 </div>""".format(request.script_root)
